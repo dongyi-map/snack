b4e939ab8078514972883c067bf8280aa424cfb3
dongyi-map <3113327431@qq.com>
2020-10-02 09:23:17 +0800

    拆分服务

---
+++ snack-eureka-server/src/main/java/com/dy/snack/eureka/App.java
@@ -0,0 +1,13 @@
+package com.dy.snack.eureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ snack-zuul-server/src/main/java/com/dy/snack/zuul/App.java
@@ -0,0 +1,13 @@
+package com.dy.snack.zuul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
